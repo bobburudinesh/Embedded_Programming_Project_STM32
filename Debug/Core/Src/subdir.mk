f93d850214c48a4b9931f1e329ff4d77bc415358
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-01-31 14:02:32 -0600

    [Implemented HAL_UART_ReceiveIT with interrupt Handling instead of blocking call]

--- Debug/Core/Src/subdir.mk
+++ Debug/Core/Src/subdir.mk
@@ -5,24 +5,24 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/UART_Interrupt_Based.c \
 ../Core/Src/it.c \
-../Core/Src/main_app.c \
 ../Core/Src/msp.c \
 ../Core/Src/syscalls.c \
 ../Core/Src/sysmem.c \
 ../Core/Src/system_stm32f4xx.c 
 
 OBJS += \
+./Core/Src/UART_Interrupt_Based.o \
 ./Core/Src/it.o \
-./Core/Src/main_app.o \
 ./Core/Src/msp.o \
 ./Core/Src/syscalls.o \
 ./Core/Src/sysmem.o \
 ./Core/Src/system_stm32f4xx.o 
 
 C_DEPS += \
+./Core/Src/UART_Interrupt_Based.d \
 ./Core/Src/it.d \
-./Core/Src/main_app.d \
 ./Core/Src/msp.d \
 ./Core/Src/syscalls.d \
 ./Core/Src/sysmem.d \
@@ -36,7 +36,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/it.cyclo ./Core/Src/it.d ./Core/Src/it.o ./Core/Src/it.su ./Core/Src/main_app.cyclo ./Core/Src/main_app.d ./Core/Src/main_app.o ./Core/Src/main_app.su ./Core/Src/msp.cyclo ./Core/Src/msp.d ./Core/Src/msp.o ./Core/Src/msp.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su
+	-$(RM) ./Core/Src/UART_Interrupt_Based.cyclo ./Core/Src/UART_Interrupt_Based.d ./Core/Src/UART_Interrupt_Based.o ./Core/Src/UART_Interrupt_Based.su ./Core/Src/it.cyclo ./Core/Src/it.d ./Core/Src/it.o ./Core/Src/it.su ./Core/Src/msp.cyclo ./Core/Src/msp.d ./Core/Src/msp.o ./Core/Src/msp.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su
 
 .PHONY: clean-Core-2f-Src
 
