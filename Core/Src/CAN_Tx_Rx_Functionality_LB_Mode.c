5489a42a62c88e299b75ed4b4d2c51f9bb94f82d
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-02-05 12:24:25 -0600

    [Added CAN Peripheral and Created CAN_Tx_Rx_Functionality_LB_Mode.c File]

---
+++ Core/Src/CAN_Tx_Rx_Functionality_LB_Mode.c
@@ -0,0 +1,13 @@
+/*
+ * CAN_Tx_Rx_Functionality_LB_Mode.c
+ *
+ *  Created on: Feb 5, 2025
+ *      Author: dinesh bobburu
+ */
+
+/*
+ * Test CAN Tx and Rx functionality using STM32 CAn(bxCAn) loop-back Mode.
+ *
+ * */
+
+
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -90,6 +90,7 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM6_Init();
   MX_TIM2_Init();
+  MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
 
 
